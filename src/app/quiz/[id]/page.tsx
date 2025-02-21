507cbcb4e22033e6dfff5293ccd018e6ce4223e5
zy123123-zy <712956400TS@protonmail.com>
2025-02-21 11:46:09 +0800

    fix: vercel bug urlParams ts;

--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -1,7 +1,12 @@
-export default function QuizPage({params}: {params: {id: string}}) {
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function QuizPage(props: Props) {
+  const { id } = await props.params;
   return (
     <section>
-      <h1>Quiz {params.id}</h1>
+      <h1>Quiz {id}</h1>
     </section>
   );
 }
