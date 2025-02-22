8db60448c1a683db6e29280e7caa0cdc38625eaf
zy123123-zy <712956400TS@protonmail.com>
2025-02-22 16:30:59 +0800

    fix: fix searchParams parans to deploy try 1;

--- src/app/components/Quizzes.tsx
+++ src/app/components/Quizzes.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import postgres from 'postgres';
 export default async function Quizzes() {
-  const sql = postgres(process.env.DATABASE_URL);
+  const sql: any = postgres(process.env.DATABASE_URL);
   type Quiz = {
     quiz_id: number;
     description: string;
@@ -10,14 +10,6 @@ export default async function Quizzes() {
   const quizzes: Quiz[] = await sql`
     SELECT quiz_id, description, title FROM quizzes
   `;
-  // const quizzes = [
-  //   { quiz_id: 1, title: 'Quiz 1' },
-  //   { quiz_id: 2, title: 'Quiz 2' },
-  //   { quiz_id: 3, title: 'Quiz 3' }
-  // ]
-  console.log(`%c======================== %cQuizzes-17 %c========================>`, 'color: #9089e3', 'color: #5e8d0a', 'color: #9089e3');
-  console.log('quizzes :', quizzes);
-  console.log(`%c======================== %cQuizzes-17 %c========================>`, 'color: #9089e3', 'color: #5e8d0a', 'color: #9089e3');
   return (
     <ul>
       {quizzes.map((quiz) => (
